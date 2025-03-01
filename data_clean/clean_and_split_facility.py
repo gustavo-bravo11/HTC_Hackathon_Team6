dba709a73d7cee4d17ce1877bb92e661b058fb37
pavobravo11 <gustavoa1011@gmail.com>
2025-03-01 00:35:29 -0700

    added new data dump

--- data_clean/clean_and_split_facility.py
+++ data_clean/clean_and_split_facility.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # Load data
-data = pd.read_csv("../data_sets/Geoscout Facility Data Dump.csv", low_memory=False)
+data = pd.read_csv("../data_sets/Facility_Data Pull.csv", low_memory=False)
 
 # Rename primary key column
 data.columns = data.columns.str.replace('Unique Facility ID', 'id')
@@ -14,18 +14,24 @@ data.columns = data.columns.str.lower().str.replace(' ', '_').str.replace('(', '
 data.dropna(subset=['id', 'current_operator', 'name'], inplace=True)
 
 # Split the data into seperate tables
-facilities = data.iloc[:, :13].drop_duplicates().reset_index(drop=True)
+data.columns = data.columns.str.replace('Unique Facility ID', 'id')
+facilities = data.iloc[:, list(range(13)) + [45, 46]].drop_duplicates().reset_index(drop=True)
 facility_capacity = data.iloc[:, [0] + list(range(13, 35))].drop_duplicates().reset_index(drop=True)
-facility_total_production = data.iloc[:, [0] + list(range(35, 45))].drop_duplicates().reset_index(drop=True)
-facility_monthly_production = data.iloc[:, [0] + list(range(45, 76))].drop_duplicates().reset_index(drop=True)
+facility_total_production = data.iloc[:, [0] + list(range(35, 80))].drop_duplicates().reset_index(drop=True)
+facility_monthly_production = data.iloc[:, [0] + list(range(80, 127))].drop_duplicates().reset_index(drop=True)
+
+# Add all data to a dictionary to loop over it and do high level analysis
+data_dict = {
+    "facilities": facilities,
+    "facility_capacity": facility_capacity,
+    "facility_total_production": facility_total_production,
+    "facility_monthly_production": facility_monthly_production
+}
 
 # Add a prefix and make the date name more appropriate
 facility_monthly_production.columns = facility_monthly_production\
     .columns.map(lambda x: 'monthly_' + x if x not in ['id', 'location', 'date', 'sub_type'] else x)
 facility_monthly_production.rename(columns={'date': 'production_month'}, inplace=True)
-
-# # This table contains no data
-# facility_inventory_meter = data.iloc[:, [0] + list(range(76, 94))].drop_duplicates().reset_index(drop=True)
 
 # Create dictionary of dataframes for easier manipulation
 data_dict = {
@@ -55,6 +61,10 @@ facility_capacity.dropna(axis=1, how='all', inplace=True)
 facility_total_production.dropna(axis=1, how='all', inplace=True)
 facility_monthly_production.dropna(axis=1, how='all', inplace=True)
 
+# Drop columns that contain only (NaN or 0)
+for df in data_dict.values():
+    df.drop(columns=df.columns[(df.eq(0) | df.isna()).all()], inplace=True)
+
 # Standardize operator names
 facilities['current_operator'] = facilities['current_operator'].replace({
     'Altagas Holdings Inc.': 'Altagas Ltd.',
@@ -80,10 +90,6 @@ facilities['current_operator'] = facilities['current_operator'].replace({
 
 data_dict['facilities']['name'] = facilities['name'].str.lstrip('* ')
 
-# Lastly, I want to add the LSD column from the monthly production data to the facility table
-lsd = facility_monthly_production[['id', 'location']].drop_duplicates().reset_index(drop=True)
-data_dict['facilities'] = facilities.merge(lsd, on='id', how='left')
-
 # Convert text fields to title case
 for name, df in data_dict.items():
     for col in df.select_dtypes(include='object').columns:
@@ -91,8 +97,8 @@ for name, df in data_dict.items():
             df[col] = df[col].apply(lambda x: x.title() if pd.notnull(x) else x)
 
 # Convert date fields
-data_dict["facilities"]["constructed_date"] = pd.to_datetime(data_dict["facilities"]["constructed_date"], format='%m/%d/%Y')
-data_dict["facility_monthly_production"]["production_month"] = pd.to_datetime(data_dict["facility_monthly_production"]["production_month"], format='%Y/%m')
+data_dict["facilities"]["constructed_date"] = pd.to_datetime(data_dict["facilities"]["constructed_date"], format='%Y-%m-%d')
+data_dict["facility_monthly_production"]["production_month"] = pd.to_datetime(data_dict["facility_monthly_production"]["production_month"], format='%b-%y')
 
 # Ensure output directory exists
 if not os.path.exists("data_cleaned"):
@@ -104,7 +110,7 @@ for name, df in data_dict.items():
         os.mkdir(f"data_cleaned/{name}")
     df.to_csv(f"data_cleaned/{name}/{name}.csv", index=False)
 
-# for df in data_dict.values():
-#     print(df.head(10))
+for df in data_dict.values():
+    print(df.head(10))
 
-print(data_dict['facilities'].head(30))
+# print(data_dict['facilities'].head(30))
